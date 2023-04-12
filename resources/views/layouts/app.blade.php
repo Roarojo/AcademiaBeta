769aa83f6dcdc692c2b047d886d4354b5aa4f1c6
Osvaldo Rene Rojo Roa <osvaldo.rojo@zapotlanejo.tecmm.edu.mx>
2023-04-12 15:45:49 -0600

    Agregando google analiti'c

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,19 @@
 </style>
 
 <!--Fin del contador-->
+<!--codigo de google analitic's-->
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0ZENLMHLGV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0ZENLMHLGV');
+</script>
+
+<!--fin del codigo de google analitic's-->
 
 
 </head>
